4302a35f3f2f5b6fe946627e3cb4b5b3a48cd49a
MilosM348 <milos.milic001@outlook.com>
2023-03-07 15:48:39 +0100

    update some parts

--- src/Controller/AboutusController.php
+++ src/Controller/AboutusController.php
@@ -3,7 +3,6 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AboutusController extends AbstractController
 {
--- src/Controller/ActiveTypeController.php
+++ src/Controller/ActiveTypeController.php
@@ -2,7 +2,6 @@
 namespace App\Controller;
       
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Constraints as Assert;
