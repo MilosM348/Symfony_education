a1415e2ed3dbfe54d63aabe4b84ed4d9c0224edf
MilosM348 <milos.milic001@outlook.com>
2023-03-07 15:49:25 +0100

    fix

--- src/Controller/ActiveTypeController.php
+++ src/Controller/ActiveTypeController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
       
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Constraints as Assert;
