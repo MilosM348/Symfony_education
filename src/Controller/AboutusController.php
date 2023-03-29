7831c60813fec8025217fe4a1c177458762929b1
MilosM348 <milos.milic001@outlook.com>
2023-03-29 20:27:35 +0200

    update about

--- src/Controller/AboutusController.php
+++ src/Controller/AboutusController.php
@@ -10,9 +10,5 @@ class AboutusController extends AbstractController
     /**
      * @Route("/aboutus", name="aboutus")
      */
-    public function index(): Response
-    {
-        return $this->render('pages/aboutus/index.html.twig', [
-        ]);
-    }
+   
 }
