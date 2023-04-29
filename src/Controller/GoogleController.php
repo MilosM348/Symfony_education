5b3b63aafef3596d44d2e05ef10959de85108c68
MilosM348 <milos.milic001@outlook.com>
2023-04-29 16:10:31 +0200

    update profile

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Component\HttpFoundation\Request;
 
 class GoogleController extends AbstractController
 {
