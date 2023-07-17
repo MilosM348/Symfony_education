191e44711d22ee74cfc1923a8ffb588ea3358b3f
MilosM348 <milos.milic001@outlook.com>
2023-07-17 16:15:44 +0200

    update user controller

--- src/Controller/UserRequestController.php
+++ src/Controller/UserRequestController.php
@@ -16,6 +16,11 @@ use \DateTime;
 
 class UserRequestController extends AbstractController
 {
+    protected $session;
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
 
     private function isAuth() {
         if(is_null($this->session->get('user'))){
